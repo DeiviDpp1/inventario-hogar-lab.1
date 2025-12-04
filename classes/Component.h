262ea99c7f2b3c57b36f12747d5acbe14b2a98e6
Felipe_Rodriguez <jfelipervargas@gmail.com>
2025-12-04 10:34:25 -0500

    Se añaden las variables, MaxQuantity y MinQuantity

--- classes/Component.h
+++ classes/Component.h
@@ -9,11 +9,13 @@ public:
     QString name;
     QString type;
     int quantity;
+    int MinQuantity;
+    int MaxQuantity;
     QString location;
     QString purchase_date;
 
     Component(){}
-    Component(int id_articulo, const QString& name, const QString type, int quantity, const QString location, const QString& purchase_date);
+    Component(int id_component, const QString& name, const QString type, int quantity, int MinQuantity, int MaxQuantity , const QString location, const QString& purchase_date);
 };
 
 #endif // COMPONENT_H
